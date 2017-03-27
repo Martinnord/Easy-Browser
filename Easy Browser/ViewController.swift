f912f38f71202fb05072e939bf4b84a41927c0cf
Martin Nordström <martinnord@Martin-Nord.local>
2017-03-27 09:34:54 +0200

    User can now choose two websites to load instead of one static

--- Easy Browser/ViewController.swift
+++ Easy Browser/ViewController.swift
@@ -15,8 +15,6 @@ class ViewController: UIViewController, WKNavigationDelegate {
     
     override func loadView() {
         
-        title = "Martin Nordström"
-        
         webView = WKWebView()
         webView.navigationDelegate = self // When any web page navigation happens, please tell me
         view = webView
@@ -24,12 +22,33 @@ class ViewController: UIViewController, WKNavigationDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
 
-        let url = URL(string: "https://martinnord.me/")!
+        /*let url = URL(string: "https://martinnord.me/")!
         webView.load(URLRequest(url: url))
-        webView.allowsBackForwardNavigationGestures = true
+        webView.allowsBackForwardNavigationGestures = true*/
     }
-
+    
+    func openTapped() {
+        let ac = UIAlertController(title: "Open page…", message: nil, preferredStyle: .actionSheet)
+        ac.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
+        ac.addAction(UIAlertAction(title: "martinnord.me", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(ac, animated: true)
+    }
+    
+    func openPage(action: UIAlertAction!) {
+        
+        let url = URL(string: "https://" + action.title!)!
+        webView.load(URLRequest(url: url))
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
